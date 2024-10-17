d412ec04922ff6f09e9f9358803bdcfd2592e955
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-10-18 05:44:56 +0600

    single double quote

--- ExercisePrinting.py
+++ ExercisePrinting.py
@@ -1,3 +1,5 @@
 print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees for 30 minut5.")
 
-
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
